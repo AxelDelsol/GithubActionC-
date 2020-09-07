304b2f8c504e3068cfff142935b501ebd84da5bf
Axel Delsol <axel.delsol@laposte.net>
2020-09-07 16:44:13 +0200

    Fixed failing tests

--- UnitTestDemo/UnitTest.Tests/UnitTest1.cs
+++ UnitTestDemo/UnitTest.Tests/UnitTest1.cs
@@ -8,7 +8,26 @@ namespace UnitTest.Tests
         [Fact]
         public void Test1()
         {
-            Assert.False(true);
+            Assert.False(false);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(2)]
+        [InlineData(4)]
+        public void Test_IsEven(int number)
+        {
+            Assert.True(UnitTestDemo.Parity.IsEven(number));
+        }
+
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(3)]
+        [InlineData(25)]
+        public void Test_IsOdd(int number)
+        {
+            Assert.True(UnitTestDemo.Parity.IsOdd(number));
         }
     }
 }
